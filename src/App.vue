69a95e6e9d8cc61d5a34fd4bcb127542d04bc38b
cccyb <chenyubo1996@gmail.com>
2016-12-27 16:47:20 +0800

    :rocket: 完成头部header组件的结构及样式编写

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-header></v-header>
+    <v-header :seller="seller"></v-header>
     <div class="tab border-1px">
       <div class="tab-item">
         <a v-link="{ path: '/goods' }">商品</a>

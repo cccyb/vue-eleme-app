11e1d84c6695af16de9b068e29f670b00c97e240
cccyb <chenyubo1996@gmail.com>
2016-12-31 03:51:58 +0800

    :wrench: 修改Eslint配置文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,6 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'semi': ['error', 'always'],
     'indent': 0,
-    "space-before-function-paren": 0
+    'space-before-function-paren': 0
   }
 }

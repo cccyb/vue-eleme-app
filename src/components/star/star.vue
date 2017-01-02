323bb00cfdbc82e99013206843604472e960d604
cccyb <chenyubo1996@gmail.com>
2017-01-02 22:24:24 +0800

    修改star组件样式

--- src/components/star/star.vue
+++ src/components/star/star.vue
@@ -69,7 +69,7 @@
       .star-item
         width: 15px
         height: 15px
-        margin-right: 16px
+        margin-right: 6px
         background-size: 15px 15px
         &:last-child
           margin-right: 0

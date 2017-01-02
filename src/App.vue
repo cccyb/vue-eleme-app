6c0de1c71b65416c93b8a6f94d02c0b2ab64edd9
cccyb <chenyubo1996@gmail.com>
2017-01-02 22:24:53 +0800

    新增本地缓存收藏状态功能

--- src/App.vue
+++ src/App.vue
@@ -12,26 +12,32 @@
         <a v-link="{ path: '/seller' }">商家</a>
       </div>
     </div>
-    <router-view :seller="seller"></router-view>
+    <router-view :seller="seller" keep-alive></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import header from 'components/header/header.vue';
+  import {urlParse} from 'common/js/util.js';
+  import header from 'components/header/header';
 
   const ERR_OK = 0;
 
   export default {
     data() {
       return {
-        seller: {}
+        seller: {
+          id: (() => {
+            let queryParam = urlParse();
+            return queryParam.id;
+          })()
+        }
       };
     },
     created() {
-      this.$http.get('/api/seller').then((response) => {
+      this.$http.get('/api/seller?id=' + this.seller.id).then((response) => {
         response = response.body;
         if (response.errno === ERR_OK) {
-          this.seller = response.data;
+          this.seller = Object.assign({}, this.seller, response.data);
         }
       });
     },
